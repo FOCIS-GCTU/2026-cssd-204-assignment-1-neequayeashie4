9cb0d004957162adc95a53cfac6686c2eb72b3aa
neequayeashie4 <2425403683@live.gctu.edu.gh>
2026-05-31 10:55:49 +0000

    "Submit completed initials"

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 2
+#
+# Name: Neequaye Yaw Ashie
+# STUDENT ID:  2425403683
+# Email: 2425403683@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Neequaye Yaw Ashie, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Print my initials in large stylized letters.
+    print()
+    print("...NYA")
+    print()
+    print("...NNN       NN........YY        YY........     AA     .....")
+    print("...NNNN      NN........ YY      YY ........    AAAA    .....")
+    print("...NN NN     NN........  YY    YY  ........   AA  AA   .....")
+    print("...NN  NN    NN........   YY  YY   ........  AA    AA  .....")
+    print("...NN   NN   NN........    YYYY    ........ AAAAAAAAAA .....")
+    print("...NN    NN  NN........     YY     ........ AA      AA .....")
+    print("...NN     NN NN........     YY     ........ AA      AA .....")
+    print("...NN      NNNN........     YY     ........ AA      AA .....")
+    print("...NN       NNN...**...     YY     ...**... AA      AA ...**")
+    print("...NN        NN...**...     YY     ...**... AA      AA ...**")
+    print()
 
 
 main()
